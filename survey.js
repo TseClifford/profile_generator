26b9b271480d4110877659d16f57574ce5af693f
Clifford Tse <TseClifford@gmail.com>
2022-01-18 23:30:42 +0000

    added profile generator

---
+++ survey.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`What's your name? Nicknames are also acceptable :) `, (name) => {
+  rl.question(`What's an activity you like doing? `, (activity) => {
+    rl.question(`What do you listen to while doing that? `, (listen) => {
+      rl.question(`Which meal is your favourite (eg: dinner, brunch, etc.) `, (meal) => {
+        rl.question(`What's your favourite thing to eat for that meal? `, (eat) => {
+          rl.question(`Which sport is your absolute favourite? `, (sport) => {
+            rl.question(`What is your superpower? In a few words, tell us what you are amazing at! `, (superpower) => {
+
+              console.log(`
+              \rYour name is ${name}. The activity that you like doing is ${activity}, while listening to ${listen}. You love to eat ${eat} for ${meal}. Your favourite sport is ${sport}, and your superpower is ${superpower}.
+              `);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
